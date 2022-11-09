70754ea7098c562d381eb14ef317a7155a609f08
kk <kkppzzah@gmail.com>
2022-11-09 23:22:42 +0800

    K线聚合：日线到月线

--- kstock/utils/candle_utils.py
+++ kstock/utils/candle_utils.py
@@ -11,6 +11,38 @@ day_to_week_time_func_map = {
     AggregateTimeType.PERIOD_END_TIME: lambda s: datetime_utils.get_week_last_day(s.iloc[0])
 }
 
+day_to_month_time_func_map = {
+    AggregateTimeType.FIRST_DATA_TIME: 'first',
+    AggregateTimeType.LAST_DATA_TIME: 'last',
+    AggregateTimeType.PERIOD_START_TIME: lambda s: datetime_utils.get_month_first_day(s.iloc[0]),
+    AggregateTimeType.PERIOD_END_TIME: lambda s: datetime_utils.get_month_last_day(s.iloc[0])
+}
+
+
+def aggregate_candles_day(
+        candles: pd.DataFrame,
+        rule: str, time_func_map: dict,
+        aggregate_time_type: AggregateTimeType = AggregateTimeType.PERIOD_START_TIME
+) -> pd.DataFrame:
+    """
+
+    :param candles:
+    :param rule:
+    :param time_func_map:
+    :param aggregate_time_type:
+    :return:
+    """
+    result_candles = candles.resample(rule, on='time').agg({
+        'time': time_func_map[aggregate_time_type],
+        'open': 'first',
+        'high': 'max',
+        'low': 'min',
+        'close': 'last',
+        'volume': 'sum',
+        'amount': 'sum',
+    })
+    return result_candles.set_index('time', drop=False)
+
 
 def aggregate_candles_day_to_week(
         candles: pd.DataFrame, aggregate_time_type: AggregateTimeType = AggregateTimeType.PERIOD_START_TIME
@@ -21,13 +53,16 @@ def aggregate_candles_day_to_week(
     :param candles:
     :return:
     """
-    weekly_candles = candles.resample('W', on='time').agg({
-        'time': day_to_week_time_func_map[aggregate_time_type],
-        'open': 'first',
-        'high': 'max',
-        'low': 'min',
-        'close': 'last',
-        'volume': 'sum',
-        'amount': 'sum',
-    })
-    return weekly_candles.set_index('time', drop=False)
+    return aggregate_candles_day(candles, 'W', day_to_week_time_func_map, aggregate_time_type=aggregate_time_type)
+
+
+def aggregate_candles_day_to_month(
+        candles: pd.DataFrame, aggregate_time_type: AggregateTimeType = AggregateTimeType.PERIOD_START_TIME
+) -> pd.DataFrame:
+    """
+    聚合日K线到周K线。
+    :param candles:
+    :param aggregate_time_type:
+    :return:
+    """
+    return aggregate_candles_day(candles, 'M', day_to_month_time_func_map, aggregate_time_type=aggregate_time_type)
